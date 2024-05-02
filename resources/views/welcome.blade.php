a5f3fd502beec5bb8a53cdbc41c583f0dbe65b3e
Fil <filongunio@gmail.com>
2024-05-03 04:16:55 +0800

    add favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <link rel="stylesheet" href="{{asset('css/nav-bar.css')}}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/Icon.png') }}">
     <script src="{{ asset('js/jquery-3.7.1.min.js') }}"></script>
     @php
         $background_url = asset('images/BG.png');
