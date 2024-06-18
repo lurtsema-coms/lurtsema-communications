d793d385e72b8c62887e265f7b0448ae029dc883
Fil <filongunio@gmail.com>
2024-06-19 01:04:52 +0800

    update responsiveness of footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,10 +1,10 @@
-<div class="container mx-auto flex flex-col lg:flex-row justify-between py-10 gap-6">
-  <div class="flex flex-row gap-5 justify-start">
+<div class="container mx-auto flex flex-col lg:flex-row justify-between items-center lg:items-start py-10 gap-6">
+  <div class="flex flex-col sm:flex-row gap-3 sm:gap-5 justify-start items-center sm:items-start">
     <p class="text-lg">All Rights Reserved {{ date('Y') }}</p>
     <a href="{{ route('terms') }}" class="text-lg underline">Terms</a>
     <a href="{{ route('privacy-policy') }}" class="text-lg underline">Privacy Policy</a>
   </div>
-  <div class="flex flex-col sm:flex-row gap-5 items-start justify-start">
+  <div class="flex flex-col sm:flex-row gap-5 items-center sm:items-start justify-start">
     <div class="flex justify-center items-center gap-3">
       <a class="hover:opacity-70 transition-all duration-200 ease-in-out" target="_blank" href="https://www.youtube.com/@LurtsemaCommunications"><img src="{{ asset('images/logo-socmed/logo-yt.png') }}" class="w-11" alt=""></a>
       <a class="hover:opacity-70 transition-all duration-200 ease-in-out" target="_blank" href="https://www.facebook.com/lurtsemacommunications"><img src="{{ asset('images/logo-socmed/logo-fb.png') }}" class="w-11" alt=""></a>
